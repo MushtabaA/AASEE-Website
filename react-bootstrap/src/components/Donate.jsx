c8d9481080ae87a3310539b63b60cb1cde691e07
Abhay2412 <57297897+Abhay2412@users.noreply.github.com>
2020-08-25 12:42:54 -0600

    Final Push of Donate Page and changes to Footer

--- react-bootstrap/src/components/Donate.jsx
+++ react-bootstrap/src/components/Donate.jsx
@@ -5,16 +5,42 @@ export default class Donate extends Component {
         return (
             <Fragment>
             <div>
-                <h1 className="title">Donate</h1>
-                <h2 className="sub">Our organization is funding by generous donations from people and organizations. We accept the following methods of donation.</h2>
+            <h1 class="title">Donate</h1>
+            <h2 class="sub">Our organization is funding by generous donations from people and organizations. We accept the following methods of donation.</h2>
             </div>
-            <div className="Payment1">
-                <h2 className="subtitle">Visa, Master Card or PayPal</h2>
-                <img src="/assets/Visa.gif" alt="" width="200"/>
+            <div class="Payment">
+            <h2 class="subtitle">Visa, Master Card or PayPal</h2>
+            <a href="https://www.paypal.com/donate/?token=EUjj0OyxQ5vXnWpeXDipMuHhM4KfQDmWcYdryoOe1X-5MNsaIMto-aKz-XzZ31cdQ92BIm&country.x=CA&locale.x=CA"><img src="/assets/Visa.gif" alt="" width="200"/></a>
+            <a href="/contact"><p class="Under"><u>Contact us for tax receipt.</u></p></a>
             </div>
-            <div className="Payment2">
-            <h2 className="subtitle">By Cheque</h2>
-                <img src="/assets/Cheque.png" alt="" width="200"/>
+            <div class="Payment">
+            <h2 class="subtitle">By Cheque</h2>
+            <a href="/contact"><img src="/assets/Cheque.png" alt="" width="200"/></a>
+            <a href="/contact"><p class="Under"><u>Contact us for tax receipt.</u></p></a>
+
+            </div>
+            <div class="Payment">
+            <h2 class="subtitle">In Person</h2>
+            <a href="/contact"><img src="/assets/donate_person.png" alt="" width="200"/></a>
+            <a href="/contact"><p class="Under"><u>Contact us for tax receipt.</u></p></a>
+
+            </div>
+            <div class="Payment">
+            <h2 class="subtitle">Canada Helps</h2>
+            <a href="https://www.canadahelps.org/en/dn/16867"><img src="/assets/donate_canadahelps.png" alt="" width="200"/></a>
+            <p class="Under">Tax receipt will be issued by Canada Helps.</p>
+
+            </div>
+            <div class="Payment">
+            <h2 class="subtitle">Aeroplan Miles</h2>
+            <a href="https://beyondmiles.aeroplan.com/eng#/"><img src="/assets/donate_aeroplan.png" alt="" width="200"/></a>
+            <p class="Under">Donate your miles.</p>
+
+            </div>
+            <div class="Payment">
+            <h2 class="subtitle">Amazon.ca</h2>
+            <img src="/assets/donate_amazon.png" alt="" width="200"/>
+            <p class="Under">Buy textbooks from AASEE's bookstore</p>
             </div>
             </Fragment>
         )
--- react-bootstrap/src/components/Footer.jsx
+++ react-bootstrap/src/components/Footer.jsx
@@ -4,27 +4,27 @@ import './Footer.css'
 
 const Footer = () => {
     return(
-        <div className="main-footer">
-            <div className ="container">
-                <div className="row">
+        <div class="main-footer">
+            <div class ="container">
+                <div class="row">
                {/* Coulmn1 */}
-               <div className = "col">
+               <div class = "col">
                    <h5>OFFICE LOCATION</h5>
-                   <ul className="list-unstyled">
+                   <ul class="list-unstyled">
                     <li>Room # ENA 229L</li>
                     <li>Schulich School of Engineering</li>
                     <li>University of Calgary</li>
                    </ul>
                </div>
                {/* Coulmn2 */}
-               <div className="col">
+               <div class="col">
                    <h4>The students were thoroughly engaged as they designed, constructed and tested their structures.
                                                                          -Teacher</h4>
                </div>
                {/* Coulmn3 */}
-               <div className = "col">
+               <div class = "col">
                    <h5>Content</h5>
-                   <ul className="list-unstyled">
+                   <ul class="list-unstyled">
                     <li><a href="/">HOME</a></li>
                     <li> <a href="/what_we_do">WHAT WE DO</a></li>
                     <li> <a href="/get_involved">GET INVOLVED</a></li>
@@ -35,8 +35,8 @@ const Footer = () => {
                </div>
                <hr />
                 </div>
-                <div className="row">
-                <p className="col-sm">
+                <div class="row">
+                <p class="col-sm">
                   Copyright  &copy;{new Date().getFullYear()}  AASEE | Privacy Policy | Site by Build Studio | Registered Charity # 1234567890
                 </p>
                 </div>
