afb2d511246f14fb6dee2f362dd9f1f08dd4e213
Abhay2412 <57297897+Abhay2412@users.noreply.github.com>
2020-08-24 23:30:24 -0600

    Adding few lines of code to css for Donate page

--- react-bootstrap/src/components/Donate.jsx
+++ react-bootstrap/src/components/Donate.jsx
@@ -1,12 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 
 export default class Donate extends Component {
     render() {
         return (
+            <Fragment>
             <div>
-                <h1>Donate</h1>
-                <h2>Our organization is funding by generous donations from people and organizations. We accept the following methods of donation.</h2>
+                <h1 className="title">Donate</h1>
+                <h2 className="sub">Our organization is funding by generous donations from people and organizations. We accept the following methods of donation.</h2>
             </div>
+            <div className="Payment1">
+                <h2 className="subtitle">Visa, Master Card or PayPal</h2>
+                <img src="/assets/Visa.gif" alt="" width="200"/>
+            </div>
+            <div className="Payment2">
+            <h2 className="subtitle">By Cheque</h2>
+                <img src="/assets/Cheque.png" alt="" width="200"/>
+            </div>
+            </Fragment>
         )
     }
 }
