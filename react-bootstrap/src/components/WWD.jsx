9bffa25a934299cba4a4519b5dd991b55c18334b
Abhay2412 <57297897+Abhay2412@users.noreply.github.com>
2020-08-25 13:12:37 -0600

    Updated the WWD -missing Sidebar

--- react-bootstrap/src/components/WWD.jsx
+++ react-bootstrap/src/components/WWD.jsx
@@ -1,11 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Jumbotron, Container, Row, Col, Image, Button } from 'react-bootstrap';
+import PropTypes from 'prop-types'
+
 
 export default class WWD extends Component {
-    render() {
-        return (
-            <div>
-                What We Do
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+          <Jumbotron fluid >
+  <Container>
+    <h1>What We Do</h1>
+  </Container>  
+</Jumbotron>
+        <img src="/assets/What.jpg" alt=""/>
+        <h1 class="pt-5 container-sm subt font-weight-bold">What We Do:</h1>
+        <p class="pt-3 container-sm subsubt">
+          The need for AASEE is clear. Enrollment in science and engineering at
+          the higher education level is declining worldwide, and Canada is no
+          exception. AASEE is working to find and address the root cause and to
+          motivate pre-university students toward science and engineering. Girls
+          are underrepresented in science and engineering at the higher
+          education level. In most of the universities and colleges in Canada
+          this number is less than 20 percent. There is much unnoticed talent in
+          the rural areas of developing countries, and with proper opportunities
+          these individuals could contribute to advancing science and technology
+          for humanity. At AASEE we target tomorrow´s leaders in science and
+          engineering who will be working with a true passion. We believe
+          advancing science and engineering education is the right way to go,
+          and the right thing to do.
+        </p>
+        <img src="/assets/Donate.png" alt=""/>
+        <h1>DONATIONS HELP KIDS LOVE TO LEARN</h1>
+          <p>AASEE is a volunteer organization helping children learn to love science and engineering. Every dollar we raise goes towards bringing innovative lessons to young Albertans.</p>
+      <img src="/assets/Donate-now.png" alt=""/>
+      </div>
+    );
+  }
 }
+
