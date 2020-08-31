b71e79a4c3b5d2f21843d9b794c36858b9c2efda
Mushtaba Al Yasseen <mushtabaalyasseen@Mushtabas-MBP.hitronhub.home>
2020-08-31 16:36:10 -0600

    n

--- react-bootstrap/src/components/Volunteers.jsx
+++ react-bootstrap/src/components/Volunteers.jsx
@@ -7,69 +7,69 @@ export default class Volunteers extends Component {
     return (
       <div>
         <div className="headTitle">
-          <h1 class="pt-5 text-center container-sm title">Volunteers</h1>
+          <h1 class="pt-5 pb-5 text-center container-sm title">Volunteers</h1>
         </div>
-        <div className="container">
-          <div className="row mb-5">
-            <div className="col-md-3">
-              <img class="img-fluid" src="/assets/v1.jpg" alt="volun1" />
+        <div className="container pt-3">
+          <div className="row">
+            <div className="col-md-3 mb-5">
+              <img class="img-fluid img-L" src="/assets/v1.jpg" alt="volun1" />
             </div>
-            <div className="col-md-3">
-              <img class="img-fluid" src="/assets/v2.jpg" alt="volun2" />
+            <div className="col-md-3 mb-5">
+              <img class="img-fluid img-L" src="/assets/v2.jpg" alt="volun2" />
             </div>
-            <div className="col-md-3">
-              <img class="img-fluid" src="/assets/v3.jpg" alt="volun3" />
+            <div className="col-md-3 mb-5">
+              <img class="img-fluid img-L" src="/assets/v3.jpg" alt="volun3" />
             </div>
-            <div className="col-md-3">
-              <img class="img-fluid" src="/assets/v4.jpeg" alt="volun4" />
+            <div className="col-md-3 mb-5">
+              <img class="img-fluid img-L" src="/assets/v4.jpeg" alt="volun4" />
             </div>
           </div>
         </div>
 
         <div className="container">
-          <div className="row  mb-5">
-            <div className="col-md-3">
-              <img class="img-fluid" src="/assets/v5.png" alt="volun5" />
+          <div className="row">
+            <div className="col-md-3 mb-5">
+              <img class="img-fluid img-L" src="/assets/v5.png" alt="volun5" />
             </div>
-            <div className="col-md-3">
-              <img class="img-fluid" src="/assets/v6.jpg" alt="volun6" />
+            <div className="col-md-3 mb-5">
+              <img class="img-fluid img-L" src="/assets/v6.jpg" alt="volun6" />
             </div>
-            <div className="col-md-3">
-              <img class="img-fluid" src="/assets/v7.jpg" alt="volun7" />
+            <div className="col-md-3 mb-5">
+              <img class="img-fluid img-L" src="/assets/v7.jpg" alt="volun7" />
             </div>
-            <div className="col-md-3">
-              <img class="img-fluid" src="/assets/v8.jpg" alt="volun8" />
-            </div>
-          </div>
-        </div>
-
-        <div className="container">
-          <div className="row  mb-5">
-            <div className="col-md-3">
-              <img class="img-fluid" src="/assets/vn.jpg" alt="volun9" />
-            </div>
-            <div className="col-md-3">
-              <img class="img-fluid" src="/assets/vn.jpg" alt="volun10" />
-            </div>
-            <div className="col-md-3">
-              <img class="img-fluid" src="/assets/vn.jpg" alt="volun11" />
-            </div>
-            <div className="col-md-3">
-              <img class="img-fluid" src="/assets/vn.jpg" alt="volun12" />
+            <div className="col-md-3 mb-5">
+              <img class="img-fluid img-L" src="/assets/v8.jpg" alt="volun8" />
             </div>
           </div>
         </div>
 
         <div className="container">
-          <div className="row  mb-5">
-            <div className="col-md-3"></div>
-            <div className="col-md-3">
-              <img class="img-fluid" src="/assets/vn.jpg" alt="volun13" />
+          <div className="row">
+            <div className="col-md-3 mb-5">
+              <img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun9" />
             </div>
-            <div className="col-md-3">
-              <img class="img-fluid" src="/assets/vn.jpg" alt="volun14" />
+            <div className="col-md-3 mb-5">
+              <img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun10" />
             </div>
-            <div className="col-md-3"></div>
+            <div className="col-md-3 mb-5">
+              <img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun11" />
+            </div>
+            <div className="col-md-3 mb-5">
+              <img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun12" />
+            </div>
+          </div>
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3 mb-5"></div>
+            <div className="col-md-3 mb-5">
+              <img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun13" />
+            </div>
+            <div className="col-md-3 mb-5">
+              <img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun14" />
+            </div>
+            <div className="col-md-3 mb-5"></div>
           </div>
         </div>
 
@@ -86,3 +86,5 @@ export default class Volunteers extends Component {
     );
   }
 }
+
+
