02943803548d9f2fea21c80b0398767cdaf47e11
Abhay2412 <57297897+Abhay2412@users.noreply.github.com>
2020-08-30 18:44:21 -0600

    Updated the What We Do Page

--- react-bootstrap/src/components/WWD.jsx
+++ react-bootstrap/src/components/WWD.jsx
@@ -7,32 +7,21 @@ export default class WWD extends Component {
   render() {
     return (
       <div>
-          <Jumbotron fluid >
-  <Container>
-    <h1>What We Do</h1>
-  </Container>  
+          <Jumbotron fluid >  
 </Jumbotron>
-        <img src="/assets/What.jpg" class="sub" alt=""/>
         <h1 class="pt-5 text-center container-sm title">What We Do:</h1>
-        <p class="pt-3 text-center container-sm subtitle">
-          The need for AASEE is clear. Enrollment in science and engineering at
-          the higher education level is declining worldwide, and Canada is no
-          exception. AASEE is working to find and address the root cause and to
-          motivate pre-university students toward science and engineering. Girls
-          are underrepresented in science and engineering at the higher
-          education level. In most of the universities and colleges in Canada
-          this number is less than 20 percent. There is much unnoticed talent in
-          the rural areas of developing countries, and with proper opportunities
-          these individuals could contribute to advancing science and technology
-          for humanity. At AASEE we target tomorrow´s leaders in science and
-          engineering who will be working with a true passion. We believe
-          advancing science and engineering education is the right way to go,
-          and the right thing to do.
+        <h2 class="pt-4 text-left container-sm title">Our Mission</h2>
+      <p class="pt-3 text-left container-sm subtitle">
+      The need for AASEE is clear. Enrollment in science and engineering at the higher education level is declining worldwide, and Canada is no exception. AASEE is working to find and address the root cause and to motivate pre-university students toward science and engineering. 
+Girls are underrepresented in science and engineering at the higher education level. 
+In most of the universities and colleges in Canada this number is less than 20 percent. 
+      </p>
+      <h2 class="pt-4 text-left container-sm title">Our Values</h2>
+        <p class="pt-3 text-left container-sm subtitle">
+        There is much unnoticed talent in the rural areas of developing countries, and with proper opportunities these individuals could contribute to advancing science and technology for humanity.  
+At AASEE we target tomorrow´s leaders in science and engineering who will be working with a true passion. 
+We believe advancing science and engineering education is the right way to go, and the right thing to do.
         </p>
-        <img src="/assets/Donate.png" class="sub" alt=""/>
-        <h1 class="pt-5 text-center container-sm title">DONATIONS HELP KIDS LOVE TO LEARN</h1>
-          <p class="pt-3 text-center container-sm subtitle">AASEE is a volunteer organization helping children learn to love science and engineering. Every dollar we raise goes towards bringing innovative lessons to young Albertans.</p>
-      <img src="/assets/Donate-now.png" class="sub" alt=""/>
       </div>
     );
   }
