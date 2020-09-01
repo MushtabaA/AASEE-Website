84d6a80deab5146fcad0eadf79cb53aa6f11bccc
Parbir Lehal <parbir@Parbirs-MacBook-Pro.local>
2020-09-01 00:25:07 -0600

    news

--- react-bootstrap/src/components/News.jsx
+++ react-bootstrap/src/components/News.jsx
@@ -6,20 +6,73 @@ export default class News extends Component {
     render() {
         return (
             <div>
-                <img src="/assets/news_header.png" class = "header" alt="news_header.png"/>
-                <img src="/assets/global.png" class = "global p-4" alt="global.png"/>
+                {/* <h1 class="pt-5 text-center container-sm title">News</h1> */}
+                <img src="/assets/news_header.png" class = "header img-fluid" alt="news_header.png"/>
+                <img src="/assets/global.png" class = "global p-4 rounded mx-auto d-block img-fluid" alt="global.png"/>
+                <hr class = "lines"/> 
+             <div class="container my-container">
+                <div class="row justify-content-between my-row">
+                    <div className="col my-col">
+                    <h1 class="pt-1 ml-2 text-left container-sm title">AASEE on the Road</h1>
+                    </div>
+                    <div className="col my-col">
+                    <h3 class="pt-0 ml- text-center container-sm subtitle">Additional Readings</h3>
+                    </div>
+                </div>
 
-             <hr class = "lines"/> 
-            <div className="NewsHeader">
-                <h1 class="pt-1 ml-2 text-left container-sm title">AASEE on the Road</h1>
-                <h5 class="pt-0 ml-2 mb-0 text-left container-sm subtitle">
-            FEB 15 2017
-                </h5>
-                <h5 class="pt-0 ml-2 text-left container-sm subtitle">
-            kevinbrennan
-                </h5>
+                <div class="row justify-content-between my-row">
+                    <div className="col my-col">
+                    <h5 class="pt-0 ml-2 mb-0 text-left container-sm subtitle">
+                         FEB 15 2017
+                     </h5>
+                    </div>
+                    <div className="col my-col">
+                        <h3 class="pt-0 ml-0 text-left container-sm subtitle">Community Initiatives Programs Grant
+                        </h3>
+                    </div>
+                </div>
+                <div className="row my-row">
+                    <div className="col my-col">
+                        <h5 class="pt-0 ml-2 text-left container-sm subtitle">
+                        By kevinbrennan
+                        </h5>
+                    </div>
+                    <div className="col my-col">
+                        <h3 class="pt-1 ml-2 text-left container-sm subtitle">Jan 25 2018 By Jessica Cohen
+                        </h3>
+                    </div>
+                    
+                </div>
+                    <div className="row my-row">
+                        <div className="col my-col">
+                        <h5 class="pt-0 ml-2 text-left container-sm subtitle">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis dolores amet ad obcaecati vitae labore, dolore quibusdam ea doloribus qui sequi itaque. Atque officiis nam mollitia harum esse alias reiciendis.
+                    </h5>
+                    </div>
+                    {/* <div className="col my-col"> <p class="pt-0 ml-2 text-left container-sm subtitle"> placeholder text </p></div> */}
+                
+                    <div className="col my-col">
+                        <p class="text-left container-sm ">AASEE would like to extend a thank you to the Community Initiatives Programs for awarding us with a grant.
 
-            </div>
+                        </p>
+                        <h5 class="pt-0 ml-2 text-center container-sm subtitle">
+                        READ MORE
+                    </h5>
+                    </div>
+                    </div>
+                     {/* <div className="row my-row">
+                        <div className="col"></div>
+                        <div className="col align-top my-col">
+                        <h5 class="pt-0 ml-2 text-center container-sm subtitle">
+                        READ MORE
+                    </h5>
+                    </div>
+                    </div>  */}
+                    
+                
+             </div>
+
+            
            </div>
         )
     }
