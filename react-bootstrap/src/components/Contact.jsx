2bc682b1a3b1fbe33abc3d02e8852afbb693d484
Mushtaba Al Yasseen <mushtabaalyasseen@Mushtabas-MBP.hitronhub.home>
2020-09-01 00:07:11 -0600

    n

--- react-bootstrap/src/components/Contact.jsx
+++ react-bootstrap/src/components/Contact.jsx
@@ -29,73 +29,76 @@ import "./Contact.css";
 export default class Contact extends Component {
   render() {
     return (
-      <div className="w-75 mx-auto d-block">
-        <h1 className="text-center mt-5 mb-3 font-weight-bold">Contact</h1>
-        <h4 className="text-center mt-5 mb-3 font-weight-bold w-25">
-          Office Location
-        </h4>
-        <h6 className="text-center  w-25">Room # ENA 229L</h6>
-        <h6 className="text-center  w-25 p-2">
-          Schulich School of Engineering
-        </h6>
-        <h6 className="text-center  w-25">University of Calgary</h6>
+      <div >
+        <div class="card map-card">
 
-        <h4 className="text-center mt-5 mb-3 font-weight-bold w-25">
-          Follow Us on{" "}
-          <a  style={ {color: 'blue'} } href="https://www.facebook.com/v2.8/plugins/error/confirm/like?iframe_referer=https%3A%2F%2Faasee.ca%2Fcontact%2F&kid_directed_site=false&secure=true&plugin=like&return_params=%7B%22action%22%3A%22like%22%2C%22app_id%22%3A%221411937785535776%22%2C%22channel%22%3A%22https%3A%2F%2Fstaticxx.facebook.com%2Fx%2Fconnect%2Fxd_arbiter%2F%3Fversion%3D46%23cb%3Df182bb1608a5ae%26domain%3Daasee.ca%26origin%3Dhttps%253A%252F%252Faasee.ca%252Ff29f88cc520711%26relation%3Dparent.parent%22%2C%22container_width%22%3A%22454%22%2C%22href%22%3A%22https%3A%2F%2Fwww.facebook.com%2FAASEE-576623595718823%2F%22%2C%22layout%22%3A%22button_count%22%2C%22locale%22%3A%22en_US%22%2C%22sdk%22%3A%22joey%22%2C%22share%22%3A%22false%22%2C%22show_faces%22%3A%22true%22%2C%22size%22%3A%22small%22%2C%22ret%22%3A%22sentry%22%2C%22act%22%3A%22like%22%7D">
-            Facebook
-          </a>{" "}
-          &{" "}
-          <a style={ {color: 'blue'} } href="https://www.linkedin.com/company/aasee-association-for-the-advancement-of-science-and-engineering-education-/">
-            LinkedIn
-          </a>
-        </h4>
+<div id="map-container-google-1" class="z-depth-1-half map-container" style="height: 500px">
+  <iframe src="https://maps.google.com/maps?q=manhatan&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0"
+    style="border:0" allowfullscreen></iframe>
+</div>
 
-        <h3 className="text-center mt-5 mb-3 font-weight-bold">Contact Us</h3>
-        <Form>
-          <Row>
-            <Col>
-              <Form.Label>First Name</Form.Label>
-              <Form.Control placeholder="First" />
-            </Col>
-            <Col>
-              <Form.Label>Last Name</Form.Label>
-              <Form.Control placeholder="Last" />
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
-            </Col>
-            <Col>
-              <Form.Group controlId="exampleForm.ControlSelect1">
-                <Form.Label>Who would you like to contact?</Form.Label>
-                <Form.Control as="select">
-                  <option>Program Coordinator</option>
-                  <option>School Programs</option>
-                  <option>Partnerships & Donations</option>
-                  <option>Donating & Purchasing Textbooks</option>
-                </Form.Control>
-              </Form.Group>
-            </Col>
-          </Row>
+<div class="card-body closed px-0">
 
-          <Form.Group controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Message</Form.Label>
-            <Form.Control as="textarea" rows="3" />
-          </Form.Group>
-          <div className="mt-3 mb-5">
-            <Button variant="primary" type="submit">
-              SEND MESSAGE
-            </Button>
-          </div>
-        </Form>
+  <div class="button px-2 mt-3">
+    <a class="btn-floating btn-lg living-coral float-right"><i class="fas fa-bicycle"></i></a>
+  </div>
+
+  <div class="white px-4 pb-4 pt-3-5">
+
+    <h5 class="card-title h5 living-coral-text">Central Park Zoo</h5>
+
+    <div class="d-flex justify-content-between living-coral-text">
+      <h6 class="card-subtitle font-weight-light">A place to relax</h6>
+      <h6 class="font-small font-weight-light mt-n1">25 min</h6>
+    </div>
+
+    <hr/>
+
+    <div class="d-flex justify-content-between pt-2 mt-1 text-center text-uppercase living-coral-text">
+      <div>
+        <i class="fas fa-phone fa-lg mb-3"></i>
+        <p class="mb-0">Call</p>
+      </div>
+      <div>
+        <i class="fas fa-cat fa-lg mb-3"></i>
+        <p class="mb-0">Love</p>
+      </div>
+      <div>
+        <i class="far fa-grin-beam-sweat fa-lg mb-3"></i>
+        <p class="mb-0">Smile</p>
+      </div>
+    </div>
+
+    <hr/>
+
+    <table class="table table-borderless">
+      <tbody>
+        <tr>
+          <th scope="row" class="px-0 pb-3 pt-2">
+              <i class="fas fa-map-marker-alt living-coral-text"></i>
+          </th>
+          <td class="pb-3 pt-2">East 64th Street, New York, NY 10021, US</td>
+        </tr>
+        <tr class="mt-2">
+          <th scope="row" class="px-0 pb-3 pt-2">
+            <i class="far fa-clock living-coral-text"></i>
+          </th>
+          <td class="pb-3 pt-2"><span class="deep-purple-text mr-2"> Closed</span> Opens 10 AM</td>
+        </tr>
+        <tr class="mt-2">
+          <th scope="row" class="px-0 pb-3 pt-2">
+            <i class="fas fa-cloud-moon living-coral-text"></i>
+          </th>
+          <td class="pb-3 pt-2">Sunny weather tomorrow</td>
+        </tr>
+      </tbody>
+    </table>
+
+  </div>
+
+</div>
+
+</div>
       </div>
     );
   }
