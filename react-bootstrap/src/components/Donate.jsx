3a389653909461e55b657e3d89e8dd55a1eba5c8
AhadAli01 <ahadali101aa@gmail.com>
2021-01-23 01:11:33 -0700

    Added responsive design for Donate page

--- react-bootstrap/src/components/Donate.jsx
+++ react-bootstrap/src/components/Donate.jsx
@@ -14,19 +14,19 @@ export default class Donate extends Component {
                 <div class="container">
                     <div class="row">
                         {/* Coulmn1 */}
-                        <div class="col">
-                            <div class="Payment">
+                        <div class="col-lg-6">
+                            <div class="Payment p-5">
                                 <h2 id="title" class="pt-3 text-center container-sm subtitle">Visa, Master Card or PayPal</h2>
                                 <a href="https://www.paypal.com/donate/?token=EUjj0OyxQ5vXnWpeXDipMuHhM4KfQDmWcYdryoOe1X-5MNsaIMto-aKz-XzZ31cdQ92BIm&country.x=CA&locale.x=CA"><img src="/assets/Visa.gif" class="sub" alt="" width="200" /></a>
                                 <a href="/contact"><p class="pt-3 text-center container-sm subtitle"><u>Contact us for tax receipt.</u></p></a>
                             </div>
-                            <div class="Payment">
+                            <div class="Payment p-5">
                                 <h2 id="title" class="pt-3 text-center container-sm subtitle">By Cheque</h2>
                                 <a href="/contact"><img src="/assets/Cheque.png" class="sub" alt="" width="200" /></a>
                                 <a href="/contact"><p class="pt-3 text-center container-sm subtitle"><u>Contact us for tax receipt.</u></p></a>
 
                             </div>
-                            <div class="Payment">
+                            <div class="Payment p-5">
                                 <h2 id="title" class="pt-3 text-center container-sm subtitle">In Person</h2>
                                 <a href="/contact"><img src="/assets/donate_person.png" class="sub" alt="" width="200" /></a>
                                 <a href="/contact"><p class="pt-3 text-center container-sm subtitle"><u>Contact us for tax receipt.</u></p></a>
@@ -34,20 +34,20 @@ export default class Donate extends Component {
                             </div>
                         </div>
                         {/* Coulmn2 */}
-                        <div class="col">
-                            <div class=" ayment">
+                        <div class="col-lg-6">
+                            <div class="Payment p-5">
                                 <h2 id="title" class="pt-3 text-center container-sm subtitle">Canada Helps</h2>
                                 <a href="https://www.canadahelps.org/en/dn/16867"><img src="/assets/donate_canadahelps.png" class="sub" alt="" width="200" /></a>
                                 <p class="pt-3 text-center container-sm subtitle">Tax receipt will be issued by Canada Helps.</p>
 
                             </div>
-                            <div class="Payment">
+                            <div class="Payment p-5">
                                 <h2 id="title" class="pt-3 text-center container-sm subtitle">Aeroplan Miles</h2>
                                 <a href="https://beyondmiles.aeroplan.com/eng#/"><img src="/assets/donate_aeroplan.png" class="sub" alt="" width="200" /></a>
                                 <p class="pt-3 text-center container-sm subtitle">Donate your miles.</p>
 
                             </div>
-                            <div class="Payment">
+                            <div class="Payment p-5">
                                 <h2 id="title" class="pt-3 text-center container-sm subtitle">Amazon.ca</h2>
                                 <img src="/assets/donate_amazon.png" class="sub" alt="" width="200" />
                                 <p class="pt-3 text-center container-sm subtitle">Buy textbooks from AASEE's bookstore</p>
