2e90ef667b920e28b2ade30f4dbfd820976000b4
AhadAli01 <ahadali101aa@gmail.com>
2021-01-23 22:04:46 -0700

    Added padding to home.jsx

--- react-bootstrap/src/components/Home.jsx
+++ react-bootstrap/src/components/Home.jsx
@@ -1,4 +1,5 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
+import { Jumbotron, Container } from "react-bootstrap";
 import "./Home.css";
 
 export default class Home extends Component {
@@ -6,7 +7,7 @@ export default class Home extends Component {
     return (
       <div>
         <div>
-        <div
+          <div
             id="carouselExampleIndicators"
             class="carousel slide"
             data-ride="carousel"
@@ -85,28 +86,28 @@ export default class Home extends Component {
               <span class="sr-only">Next</span>
             </a>
           </div>
-          <div>
-          <h1 class="pt-5 text-center container-sm title">
-            Our Mission
+          <div class="container-fluid px-2 pb-5 mb-5">
+            <h1 class="pt-5 text-center container-sm title">
+              Our Mission
           </h1>
-          <h5 id="midtitle" class="pt-5 text-center container-sm midtitle">
-            The Association for the Advancement of Science and Engineering
-            Education (AASEE) is dedicated to advancing science and engineering
-            education at the K-12 level.
+            <h5 id="midtitle" class="pt-5 text-center container-sm midtitle mb-5">
+              The Association for the Advancement of Science and Engineering
+              Education (AASEE) is dedicated to advancing science and engineering
+              education at the K-12 level.
           </h5>
-          <h6 class="subtitle">
-            The Association for the Advancement of Science and Engineering
-            Education (AASEE) seeks to inspire future science and engineering
-            leaders. AASEE gives priority to diverse populations, namely
-            Canada’s first nations peoples, girls, and those in financial need,
-            as well as rural populations. We are mandated to work across the
-            globe, and are implementing an ambitious plan. To do so, AASEE
-            designs educational outreach workshops at the K-12 level and travels
-            to specific schools and school boards. The lessons the volunteers
-            teach are curriculum-relevant, hands-on activities, which are meant
-            to engage the student, along with teachers. The Association for the
-            Advancement of Science and Engineering Education is a registered
-            charity in Canada.
+            <h6 class="subtitle px-5 mx-5 mt-5">
+              The Association for the Advancement of Science and Engineering
+              Education (AASEE) seeks to inspire future science and engineering
+              leaders. AASEE gives priority to diverse populations, namely
+              Canada’s first nations peoples, girls, and those in financial need,
+              as well as rural populations. We are mandated to work across the
+              globe, and are implementing an ambitious plan. To do so, AASEE
+              designs educational outreach workshops at the K-12 level and travels
+              to specific schools and school boards. The lessons the volunteers
+              teach are curriculum-relevant, hands-on activities, which are meant
+              to engage the student, along with teachers. The Association for the
+              Advancement of Science and Engineering Education is a registered
+              charity in Canada.
           </h6>
           </div>
           <div className="vector1">
@@ -116,7 +117,7 @@ export default class Home extends Component {
             <img src="/assets/vector2.png" alt="vector2" />
           </div>
         </div>
-        <div>
+        <div class="mb-5">
           <h1 class="pt-5 text-center container-sm title">
             Donors, Partners & Friends
           </h1>
