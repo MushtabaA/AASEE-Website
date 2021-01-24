e30ea9332d9fd6cbc723d5525a44a6ed9cd01447
AhadAli01 <ahadali101aa@gmail.com>
2021-01-23 22:55:17 -0700

    Added svg background

--- react-bootstrap/src/components/Volunteers.jsx
+++ react-bootstrap/src/components/Volunteers.jsx
@@ -6,95 +6,96 @@ export default class Volunteers extends Component {
   render() {
     return (
       <div>
-        <div class="bg"></div>
-        <div className="headTitle">
-          <h1 class="pt-5 pb-5 text-center container-sm title">Volunteers</h1>
-        </div>
-        <div className="container pt-3">
-          <div className="row">
-            <div className="col-md-3 mb-5">
-            <div class="piccontainer"><img class="img-fluid img-L" src="/assets/v1.jpg" alt="volun1" /><div class="overlay"><div class="text">Priyanka Gautam, <br/> Lead Media Relations, <br/> Fund Raising Member</div></div></div>
+        <div class="bg">
+          <div className="headTitle">
+            <h1 class="pt-5 pb-5 text-center container-sm title">Volunteers</h1>
+          </div>
+          <div className="container pt-3">
+            <div className="row">
+              <div className="col-md-3 mb-5">
+                <div class="piccontainer"><img class="img-fluid img-L" src="/assets/v1.jpg" alt="volun1" /><div class="overlay"><div class="text">Priyanka Gautam, <br /> Lead Media Relations, <br /> Fund Raising Member</div></div></div>
+              </div>
+              <div className="col-md-3 mb-5">
+                <div class="piccontainer"><img class="img-fluid img-L" src="/assets/v2.jpg" alt="volun2" /><div class="overlay"><div class="text">Andrea Aguirre, <br /> Program Prep Lead, <br /> Office Member</div></div></div>            </div>
+              <div className="col-md-3 mb-5">
+                <div class="piccontainer"><img class="img-fluid img-L" src="/assets/v3.jpg" alt="volun3" /><div class="overlay"><div class="text">Rigel Tormon, <br /> Fund Raising Member</div></div></div>            </div>
+              <div className="col-md-3 mb-5">
+                <div class="piccontainer"><img class="img-fluid img-L" src="/assets/v4.jpeg" alt="volun4" /><div class="overlay"><div class="text">Whitney Ebose, <br /> Book Store Member</div></div></div>            </div>
             </div>
-            <div className="col-md-3 mb-5">
-            <div class="piccontainer"><img class="img-fluid img-L" src="/assets/v2.jpg" alt="volun2" /><div class="overlay"><div class="text">Andrea Aguirre, <br/> Program Prep Lead, <br/> Office Member</div></div></div>            </div>
-            <div className="col-md-3 mb-5">
-            <div class="piccontainer"><img class="img-fluid img-L" src="/assets/v3.jpg" alt="volun3" /><div class="overlay"><div class="text">Rigel Tormon, <br/> Fund Raising Member</div></div></div>            </div>
-            <div className="col-md-3 mb-5">
-            <div class="piccontainer"><img class="img-fluid img-L" src="/assets/v4.jpeg" alt="volun4" /><div class="overlay"><div class="text">Whitney Ebose, <br/> Book Store Member</div></div></div>            </div>
           </div>
-        </div>
 
-        <div className="container">
-          <div className="row">
-            <div className="col-md-3 mb-5">
-            <div class="piccontainer"><img class="img-fluid img-L" src="/assets/v5.png" alt="volun5" /><div class="overlay"><div class="text">Mandeep Pandey</div></div></div>            </div>
-            <div className="col-md-3 mb-5">
-            <div class="piccontainer"><img class="img-fluid img-L" src="/assets/v6.jpg" alt="volun6" /><div class="overlay"><div class="text">Ahmad Awan, <br/> Book Store Lead</div></div></div>            </div>
-            <div className="col-md-3 mb-5">
-            <div class="piccontainer"><img class="img-fluid img-L" src="/assets/v7.jpg" alt="volun7" /><div class="overlay"><div class="text">Sienna Prince-McPherson</div></div></div>            </div>
-            <div className="col-md-3 mb-5">
-            <div class="piccontainer"><img class="img-fluid img-L" src="/assets/v8.jpg" alt="volun8" /><div class="overlay"><div class="text">Anis Haque, <br/> Fund Raising Lead</div></div></div>            </div>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-3 mb-5">
+                <div class="piccontainer"><img class="img-fluid img-L" src="/assets/v5.png" alt="volun5" /><div class="overlay"><div class="text">Mandeep Pandey</div></div></div>            </div>
+              <div className="col-md-3 mb-5">
+                <div class="piccontainer"><img class="img-fluid img-L" src="/assets/v6.jpg" alt="volun6" /><div class="overlay"><div class="text">Ahmad Awan, <br /> Book Store Lead</div></div></div>            </div>
+              <div className="col-md-3 mb-5">
+                <div class="piccontainer"><img class="img-fluid img-L" src="/assets/v7.jpg" alt="volun7" /><div class="overlay"><div class="text">Sienna Prince-McPherson</div></div></div>            </div>
+              <div className="col-md-3 mb-5">
+                <div class="piccontainer"><img class="img-fluid img-L" src="/assets/v8.jpg" alt="volun8" /><div class="overlay"><div class="text">Anis Haque, <br /> Fund Raising Lead</div></div></div>            </div>
+            </div>
           </div>
-        </div>
 
-        <div className="container">
-          <div className="row">
-            <div className="col-md-3 mb-5">
-            <div class="piccontainer"><img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun9" /><div class="overlay"><div class="text">Alden Lien, <br/> Program Prep Member, <br/> Book Store Member</div></div></div>            </div>
-            <div className="col-md-3 mb-5">
-            <div class="piccontainer"><img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun10" /><div class="overlay"><div class="text">Nicolas Vuong, <br/> Office Lead, <br/> Program Prep Member</div></div></div>            </div>
-            <div className="col-md-3 mb-5">
-            <div class="piccontainer"><img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun11" /><div class="overlay"><div class="text">Nicolas Teng, <br/> Program Prep Member, <br/> Book Store Member</div></div></div>            </div>
-            <div className="col-md-3 mb-5">
-            <div class="piccontainer"><img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun12" /><div class="overlay"><div class="text">Helen Zhang, <br/> Book Store Member</div></div></div>            </div>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-3 mb-5">
+                <div class="piccontainer"><img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun9" /><div class="overlay"><div class="text">Alden Lien, <br /> Program Prep Member, <br /> Book Store Member</div></div></div>            </div>
+              <div className="col-md-3 mb-5">
+                <div class="piccontainer"><img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun10" /><div class="overlay"><div class="text">Nicolas Vuong, <br /> Office Lead, <br /> Program Prep Member</div></div></div>            </div>
+              <div className="col-md-3 mb-5">
+                <div class="piccontainer"><img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun11" /><div class="overlay"><div class="text">Nicolas Teng, <br /> Program Prep Member, <br /> Book Store Member</div></div></div>            </div>
+              <div className="col-md-3 mb-5">
+                <div class="piccontainer"><img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun12" /><div class="overlay"><div class="text">Helen Zhang, <br /> Book Store Member</div></div></div>            </div>
+            </div>
           </div>
-        </div>
 
-        <div className="container">
-          <div className="row">
-            <div className="col-md-3 mb-5">
-            <div class="piccontainer"><img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun13" /><div class="overlay"><div class="text">Harsh Sharma, <br/> Web Development</div></div></div>            </div>
-            <div className="col-md-3 mb-5">
-            <div class="piccontainer"><img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun14" /><div class="overlay"><div class="text">Mushtaba Al Yasseen, <br/> Web Development</div></div></div>            </div>
-            <div className="col-md-3 mb-5">
-            <div class="piccontainer"><img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun15" /><div class="overlay"><div class="text">Ahad Ali, <br/> Web Development</div></div></div>            </div>
-            <div className="col-md-3 mb-5">
-            <div class="piccontainer"><img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun16" /><div class="overlay"><div class="text">Abhay Khosla, <br/> Web Development</div></div></div>            </div>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-3 mb-5">
+                <div class="piccontainer"><img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun13" /><div class="overlay"><div class="text">Harsh Sharma, <br /> Web Development</div></div></div>            </div>
+              <div className="col-md-3 mb-5">
+                <div class="piccontainer"><img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun14" /><div class="overlay"><div class="text">Mushtaba Al Yasseen, <br /> Web Development</div></div></div>            </div>
+              <div className="col-md-3 mb-5">
+                <div class="piccontainer"><img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun15" /><div class="overlay"><div class="text">Ahad Ali, <br /> Web Development</div></div></div>            </div>
+              <div className="col-md-3 mb-5">
+                <div class="piccontainer"><img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun16" /><div class="overlay"><div class="text">Abhay Khosla, <br /> Web Development</div></div></div>            </div>
+            </div>
           </div>
-        </div>
 
-        <div className="container">
-          <div className="row">
-            <div className="col-md-3 mb-5">
-            <div class="piccontainer"><img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun17" /><div class="overlay"><div class="text">Parbir Lehal, <br/> Web Development</div></div></div>            </div>
-            <div className="col-md-3 mb-5">
-            <div class="piccontainer"><img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun18" /><div class="overlay"><div class="text">Aninda Shome, <br/> Web Development</div></div></div>            </div>
-            <div className="col-md-3 mb-5">
-            <div class="piccontainer"><img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun19" /><div class="overlay"><div class="text">Rahat Islam, <br/> Web Development</div></div></div>            </div>
-            <div className="col-md-3 mb-5">
-            <div class="piccontainer"><img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun20" /><div class="overlay"><div class="text">Raiyan Kabir, <br/> Web Development</div></div></div>            </div>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-3 mb-5">
+                <div class="piccontainer"><img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun17" /><div class="overlay"><div class="text">Parbir Lehal, <br /> Web Development</div></div></div>            </div>
+              <div className="col-md-3 mb-5">
+                <div class="piccontainer"><img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun18" /><div class="overlay"><div class="text">Aninda Shome, <br /> Web Development</div></div></div>            </div>
+              <div className="col-md-3 mb-5">
+                <div class="piccontainer"><img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun19" /><div class="overlay"><div class="text">Rahat Islam, <br /> Web Development</div></div></div>            </div>
+              <div className="col-md-3 mb-5">
+                <div class="piccontainer"><img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun20" /><div class="overlay"><div class="text">Raiyan Kabir, <br /> Web Development</div></div></div>            </div>
+            </div>
           </div>
-        </div>
 
-        <div className="container">
-          <div className="row pb-5">
-            <div className="col-md-3 mb-5"></div>
-            <div className="col-md-3 mb-5">
-            <div class="piccontainer"><img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun13" /><div class="overlay"><div class="text">Mohammad Usman Khan, <br/> School Liaison Member</div></div></div>            </div>
-            <div className="col-md-3 mb-5">
-            <div class="piccontainer"><img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun14" /><div class="overlay"><div class="text">Farin Tabassum, <br/> School Liaison Lead</div></div></div>            </div>
-            <div className="col-md-3 mb-5"></div>
+          <div className="container">
+            <div className="row pb-5">
+              <div className="col-md-3 mb-5"></div>
+              <div className="col-md-3 mb-5">
+                <div class="piccontainer"><img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun13" /><div class="overlay"><div class="text">Mohammad Usman Khan, <br /> School Liaison Member</div></div></div>            </div>
+              <div className="col-md-3 mb-5">
+                <div class="piccontainer"><img class="img-fluid img-L" src="/assets/vn.jpg" alt="volun14" /><div class="overlay"><div class="text">Farin Tabassum, <br /> School Liaison Lead</div></div></div>            </div>
+              <div className="col-md-3 mb-5"></div>
+            </div>
           </div>
-        </div>
 
-        <div class="centered mt-sm-full mt-full pb-5"> 
-          <a
-            href="mailto:aaseewebsite@mail.com"
-            title="Contact for more details"
-            class="btnlink"
-          >
-            Click To Volunteer!
+          <div class="centered mt-sm-full mt-full pb-5">
+            <a
+              href="mailto:aaseewebsite@mail.com"
+              title="Contact for more details"
+              class="btnlink"
+            >
+              Click To Volunteer!
           </a>
+          </div>
         </div>
       </div>
     );
