8dfb5b8aa75f72cd90afadaae6444eb77a4ea2b5
Mushtaba Al Yasseen <mushtabaalyasseen@Mushtabas-MacBook-Pro.local>
2021-01-24 19:45:53 -0700

    Finally fixed navbar

--- react-bootstrap/src/components/Top_Navbar.jsx
+++ react-bootstrap/src/components/Top_Navbar.jsx
@@ -17,11 +17,151 @@ import "./Top_Navbar.css";
 export default class Top_Navbar extends Component {
     render() {
         return (
-            <Navbar className="navbar navbar-expand-sm navbar-custom sticky content sticky-element" collapseOnSelect expand="lg" variant="light">
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="mr-auto">
-                <Nav.Link eventKey={1} componentClass={Link} href="/" to="/">HOME</Nav.Link>
+      //       <Navbar className="sticky content sticky-element" collapseOnSelect expand="lg" variant="light">
+      //       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      //       <Navbar.Collapse id="responsive-navbar-nav">
+      //         <Nav className="mr-auto">
+      //           <Nav.Link eventKey={1} componentClass={Link} href="/" to="/">HOME</Nav.Link>
+      //           <Dropdown as={ButtonGroup}>
+      //                 <Button variant="dropdown-toggle" href="/what_we_do">WHAT WE DO</Button>
+
+      //         <Dropdown.Toggle
+      //           split
+      //           variant="dropdown-toggle"
+      //           id="dropdown-split-basic"
+      //         />
+
+      //         <Dropdown.Menu>
+      //           <Dropdown.Item
+      //             eventKey={3}
+      //             componentClass={Link}
+      //             href="/Strategies"
+      //             to="/Strategies"
+      //           >
+      //             Strategies
+      //           </Dropdown.Item>
+      //           <Dropdown.Item
+      //             eventKey={4}
+      //             componentClass={Link}
+      //             href="/Classroom"
+      //             to="/Classroom"
+      //           >
+      //             Classroom Activties
+      //           </Dropdown.Item>
+      //         </Dropdown.Menu>
+      //       </Dropdown>
+      //       <Dropdown as={ButtonGroup}>
+      //         <Button variant="dropdown-toggle" href="/get_involved">
+      //           GET INVOLVED
+      //         </Button>
+
+      //         <Dropdown.Toggle
+      //           split
+      //           variant="dropdown-toggle"
+      //           id="dropdown-split-basic"
+      //         />
+
+      //         <Dropdown.Menu>
+      //           <Dropdown.Item
+      //             eventKey={3}
+      //             componentClass={Link}
+      //             href="/get_involved/#Volunteer"
+      //             to="/get_involved/#Volunteer"
+      //           >
+      //             Be a Volunteer
+      //           </Dropdown.Item>
+      //           <Dropdown.Item
+      //             eventKey={4}
+      //             componentClass={Link}
+      //             href="/get_involved/#Partner"
+      //             to="/get_involved/#Partner"
+      //           >
+      //             Be our Partner
+      //           </Dropdown.Item>
+      //         </Dropdown.Menu>
+      //       </Dropdown>
+      //     </Nav>
+      //   </Navbar.Collapse>
+      //   <Navbar.Brand href="/" to="/">
+      //     <img
+      //       id="logo-navbar-middle"
+      //       src="/assets/HeaderLogo.png"
+      //       width="300"
+      //       alt=""
+      //     />
+      //   </Navbar.Brand>
+      //   <Navbar.Collapse id="responsive-navbar-nav">
+      //     <Nav>
+      //       <Dropdown as={ButtonGroup}>
+      //         <Button variant="dropdown-toggle" href="/about">
+      //           ABOUT
+      //         </Button>
+
+      //         <Dropdown.Toggle
+      //           split
+      //           variant="dropdown-toggle"
+      //           id="dropdown-split-basic"
+      //         />
+
+      //         <Dropdown.Menu>
+      //           <Dropdown.Item
+      //             eventKey={5}
+      //             componentClass={Link}
+      //             href="/news"
+      //             to="/news"
+      //           >
+      //             News
+      //           </Dropdown.Item>
+      //           <Dropdown.Item
+      //             eventKey={6}
+      //             componentClass={Link}
+      //             href="/volunteers"
+      //             to="/volunteers"
+      //           >
+      //             Volunteers
+      //           </Dropdown.Item>
+      //           <Dropdown.Item
+      //             eventKey={7}
+      //             componentClass={Link}
+      //             href="/BOD"
+      //             to="/BOD"
+      //           >
+      //             Board of Directors
+      //           </Dropdown.Item>
+      //         </Dropdown.Menu>
+      //       </Dropdown>
+      //       <Nav.Link
+      //         eventKey={8}
+      //         componentClass={Link}
+      //         href="/contact"
+      //         to="/contact"
+      //       >
+      //         CONTACT
+      //       </Nav.Link>
+      //       <Nav.Link
+      //         eventKey={9}
+      //         componentClass={Link}
+      //         href="/donate"
+      //         to="/donate"
+      //       >
+      //         DONATE
+      //       </Nav.Link>
+      //     </Nav>
+      //   </Navbar.Collapse>
+      // </Navbar>
+      <Navbar className="color-nav" collapseOnSelect expand="lg" variant="dark" sticky="top">
+  <Navbar.Brand href="/" to="/">
+  <img
+        src="/assets/HeaderLogo.png"
+        width="300"
+        className="d-inline-block align-top"
+        alt="AASEE Home Logo"
+      />
+  </Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+    <Nav.Link eventKey={1} componentClass={Link} href="/" to="/">HOME</Nav.Link>
                 <Dropdown as={ButtonGroup}>
                       <Button variant="dropdown-toggle" href="/what_we_do">WHAT WE DO</Button>
 
@@ -80,19 +220,10 @@ export default class Top_Navbar extends Component {
                 </Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
-          </Nav>
-        </Navbar.Collapse>
-        <Navbar.Brand href="/" to="/">
-          <img
-            id="logo-navbar-middle"
-            src="/assets/HeaderLogo.png"
-            width="300"
-            alt=""
-          />
-        </Navbar.Brand>
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav>
-            <Dropdown as={ButtonGroup}>
+    </Nav>
+    <Nav className="mr-auto">
+
+    <Dropdown as={ButtonGroup}>
               <Button variant="dropdown-toggle" href="/about">
                 ABOUT
               </Button>
@@ -146,9 +277,12 @@ export default class Top_Navbar extends Component {
             >
               DONATE
             </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
     );
   }
 }
+
+
+
