b2a2ceea4210b9b01f3a96351b09bdfa475eb504
Parbir <parbir.lehal@gmail.com>
2021-01-27 18:04:57 -0700

    Fixed News page

--- react-bootstrap/src/components/News.jsx
+++ react-bootstrap/src/components/News.jsx
@@ -97,12 +97,12 @@ export default class News extends Component {
               <h5 class="pt-0 ml-0 text-left container-sm date">
                 By kevinbrennan
               </h5>
-              <p class="pt-0 ml-0 pr-0 mr-0 text-left container-sm left">
+              <p class="pt-0 ml-0 pr-0 mr-0 text-left container-sm lato2 left">
                 Grande Prairie’s Daily Herald-Tribune recently featured AASEE as it was beginning its trip to visit five schools in the city to promote and spark an interest in the fields of science and engineering. Beginning with the Swanavon School, Dr. Anis Haque, chairman and founder of AASEE, along with graduate students Leanne Dawson and Sarah Bannister from the University of Calgary, will offer a series of workshops from grades 3 to 11 on subjects including buoyancy, electricity, robotic arms, among others.
               </p>
             </div>
             <div className="col-md-4">
-              <h3 class="pt-0 ml-0 pb-0 mb-0 text-center container-sm quick">
+              <h3 class="pt-0 ml-0 pb-2 mb-0 text-center container-sm quick">
                 Additional Readings
               </h3>
               <h2 class="pt-3 ml-2 mt-0 text-left container-sm nobold">
@@ -116,20 +116,21 @@ export default class News extends Component {
                 Initiatives Programs for awarding us with a grant.
               </h1>
 
-              <a href="https://aasee.ca/2018/01/25/community-initiatives-programs-grant/" class="pt-0 ml-2 mb-5 text-center container-sm more">
+              <a href="https://aasee.ca/2018/01/25/community-initiatives-programs-grant/" class="pt-0 pb-2 ml-2 mb-5 text-center container-sm more">
                 FIND OUT MORE
               </a>
-              <h2 class="pt-1 ml-0 mt-0 text-left container-sm nobold">
+              <hr class="lines2" />
+              <h2 class="pt-2 ml-2 mt-0 text-left container-sm nobold">
                 NSERC PromoScience Grant
               </h2>
-              <h2 class="pb-0 ml-0 mt-0 text-left container-sm lato">
+              <h2 class="pb-0 ml-2 mt-0 text-left container-sm lato">
                 Nov 20 2018 By Jessica Cohen
               </h2>
-              <h2 class="pb-1 ml-0 mt-0 text-left container-sm lato">
+              <h2 class="pb-1 ml-2 mt-0 text-left container-sm lato">
                 AASEE has been awarded a 2018 Grant to help expand our
                 educational initiatives.
               </h2>
-              <a href="https://aasee.ca/2018/11/20/nserc-promoscience-grant/" class="pt-0 ml-2 mb-5 text-center container-sm more">
+              <a href="https://aasee.ca/2018/11/20/nserc-promoscience-grant/" class="pt-0 ml-2 mb-5 container-sm more">
                 FIND OUT MORE
               </a>
             </div>
