4799e75624d0265f5284219e2322519945b43b80
Abhay2412 <57297897+Abhay2412@users.noreply.github.com>
2021-02-01 00:31:25 -0700

    Fixed the WWD Page with adding vector and padding

--- react-bootstrap/src/pages/WWD.jsx
+++ react-bootstrap/src/pages/WWD.jsx
@@ -5,37 +5,25 @@ export default class WWD extends Component {
     return (
       
       <div>
-         <section
-            id="whatwedo-section"
-            className="backstyle"
-          >
+        <div className="vector1">
+            <img src="/assets/vector1.png" alt="vector1" />
+          </div>
         <div className="d-flex justify-content-center">
        <img src="/assets/WhatWeHeader.png" class = "header img-fluid" alt="WhatWeHeader.png"/>
        </div>
-        <h1 class="title">What We Do</h1>
+        <h1 class="titleWWD">What We Do</h1>
         <h2 id ="subtitle" class="subtitle">Our Mission</h2>   
-      <p class="sub">
+      <p class="subWWD">
       The need for AASEE is clear. Enrollment in science and engineering at the higher education level is declining worldwide, and Canada is no exception. AASEE is working to find and address the root cause and to motivate pre-university students toward science and engineering. 
 Girls are underrepresented in science and engineering at the higher education level. 
 In most of the universities and colleges in Canada this number is less than 20 percent. 
       </p>
-       
-      </section>
-      <img src="/assets/1.jpg" class="whatWeDoImage1" alt="what we do">
-        </img>
       <h2 id = "subtitle" class="subtitle">Our Values</h2>
-        <p class="sub">
+        <p class="subWWD">
         There is much unnoticed talent in the rural areas of developing countries, and with proper opportunities these individuals could contribute to advancing science and technology for humanity.  
 At AASEE we target tomorrow´s leaders in science and engineering who will be working with a true passion. 
 We believe advancing science and engineering education is the right way to go, and the right thing to do.
         </p>
-        <section
-            id="whatwedo-section"
-            className="backstyle"
-          >
-        <img src="/assets/4.jpg" class="whatWeDoImage2" alt="what we do">
-        </img>
-        </section>
       </div>
       
     );
